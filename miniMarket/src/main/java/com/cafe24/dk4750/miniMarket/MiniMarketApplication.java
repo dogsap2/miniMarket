257a2ac863b55a5ab6c4d9511a2f61f9c5fcb7f4
dk4750 <dk4750@naver.com>
2020-06-29 17:41:58 +0900

    [test]

---
+++ miniMarket/src/main/java/com/cafe24/dk4750/miniMarket/MiniMarketApplication.java
@@ -0,0 +1,13 @@
+package com.cafe24.dk4750.miniMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniMarketApplication.class, args);
+	}
+
+}
---
+++ miniMarket/src/test/java/com/cafe24/dk4750/miniMarket/MiniMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafe24.dk4750.miniMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
