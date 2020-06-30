8ae2d6862c5aae0868b3642edbb60a96a0b0ac82
JungYuSeok <dmehtk@gmail.com>
2020-06-30 17:18:59 +0900

    [정유석, 김동희] report.html 멤버by멤버게시글 신고 jquery 추가
    1. 메뉴 넘길 때 값이 안 넘어가던 오류 수정(jquery 수정)
    2. view에 title메뉴에 맞는 content메뉴 추가
    3. reportTitle 값 전송 완료
    4. reportContent 값 전송 완료
    5. reportMemo 값이 있을 때와 없을 때 조건문 처리
    6. 페이지에서 컨트롤러로 값 넘어가는거 확인(디버깅 처리완료)
    7. view에 title메뉴에 맞는 content 메뉴 추가 및 '신고 항목 제안' 메뉴 클릭시 textarea 생성
    8. 이전 버튼 오류 수정 예정

--- miniMarket/src/main/java/com/cafe24/dk4750/miniMarket/controller/ExampleClass.java
+++ miniMarket/src/main/java/com/cafe24/dk4750/miniMarket/controller/ExampleClass.java
@@ -1,5 +1,22 @@
 package com.cafe24.dk4750.miniMarket.controller;
 
-public class ExampleClass {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.cafe24.dk4750.miniMarket.vo.ReportMByMitem;
+
+@Controller
+public class ExampleClass {
+		@GetMapping("/report")
+		public String report() {
+			
+			return "report";
+		}
+		@PostMapping("/report")
+		public String report(ReportMByMitem reportMByItem) {
+			System.out.println(reportMByItem);
+			return "redirect:/report";
+		}
 }
