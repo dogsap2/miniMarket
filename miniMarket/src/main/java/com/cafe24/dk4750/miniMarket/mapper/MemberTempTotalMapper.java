73485f5e0bf19ac35d89e069cfdc9bc8c725da1b
dk4750 <dk4750@naver.com>
2020-07-07 10:40:01 +0900

    [이슬민, 서일태] 매너평가 결과 매너온도에 반영 구현 완료 1. addManner.html 	- 비매너 클릭시 고정값인 -1값으로 수정 2. memberTempMapper.xml 주석 생성 3. memberTempTotalMapper.xml 	- 현재 멤버의 템프토탈 구하는 쿼리 작성 	- 멤버의 새로운 템프토탈 인서트하는 쿼리 작성 4. memberTempTotalMapper 	- 현재 멤버의 템프토탈 구하는 메소드 생성 	- 멤버의 새로운 템프토탈 인서트하는 메소드 생성 5. MannerService 	- 현재 멤버의 템프토탈을 select한 후 insert스코어값을 합산해 새로운 템프토탈 인서트

--- miniMarket/src/main/java/com/cafe24/dk4750/miniMarket/mapper/MemberTempTotalMapper.java
+++ miniMarket/src/main/java/com/cafe24/dk4750/miniMarket/mapper/MemberTempTotalMapper.java
@@ -1,8 +1,15 @@
 package com.cafe24.dk4750.miniMarket.mapper;
 
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MemberTempTotalMapper {
-
+	
+	// 현재 멤버의 temp total 구하기
+	public double selectTempTotalNow(String memberUniqueNo);
+	
+	// 새로운 템프토탈 인서트하기
+	public int insertTempTotal(Map<String, Object> map);
 }
