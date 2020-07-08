0a638d2e624a2e4c4e10a98a497482728ce9d51a
yoona misun <hya7835@naver.com>
2020-07-08 15:14:03 +0900

    [한윤아,엄미선] LoginCompany 이름변경

--- miniMarket/src/main/java/com/cafe24/dk4750/miniMarket/vo/LoginCompany.java
+++ miniMarket/src/main/java/com/cafe24/dk4750/miniMarket/vo/LoginCompany.java
@@ -5,9 +5,9 @@ public class LoginCompany {
 	private String companyPw;
 	private String companyName;	 
 	private String companyUniqueNo; 
-	private String memberSido;
-	private String memberSigungu;
-	private String memberBname;
+	private String companySido;
+	private String companySigungu;
+	private String companyBname;
 	
 	public String getCompanyId() {
 		return companyId;
@@ -15,17 +15,17 @@ public class LoginCompany {
 	public void setCompanyId(String companyId) {
 		this.companyId = companyId;
 	}
-	public String getCompanyName() {
-		return companyName;
-	}
-	public void setCompanyName(String companyName) {
-		this.companyName = companyName;
-	}
 	public String getCompanyPw() {
 		return companyPw;
 	}
 	public void setCompanyPw(String companyPw) {
 		this.companyPw = companyPw;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
 	}
 	public String getCompanyUniqueNo() {
 		return companyUniqueNo;
@@ -33,31 +33,29 @@ public class LoginCompany {
 	public void setCompanyUniqueNo(String companyUniqueNo) {
 		this.companyUniqueNo = companyUniqueNo;
 	}
-	public String getMemberSido() {
-		return memberSido;
+	public String getCompanySido() {
+		return companySido;
 	}
-	public void setMemberSido(String memberSido) {
-		this.memberSido = memberSido;
+	public void setCompanySido(String companySido) {
+		this.companySido = companySido;
 	}
-	public String getMemberSigungu() {
-		return memberSigungu;
+	public String getCompanySigungu() {
+		return companySigungu;
 	}
-	public void setMemberSigungu(String memberSigungu) {
-		this.memberSigungu = memberSigungu;
+	public void setCompanySigungu(String companySigungu) {
+		this.companySigungu = companySigungu;
 	}
-	public String getMemberBname() {
-		return memberBname;
+	public String getCompanyBname() {
+		return companyBname;
 	}
-	public void setMemberBname(String memberBname) {
-		this.memberBname = memberBname;
+	public void setCompanyBname(String companyBname) {
+		this.companyBname = companyBname;
 	}
 	
 	@Override
 	public String toString() {
-		return "LoginCompany [companyId=" + companyId + ", companyName=" + companyName + ", companyPw=" + companyPw
-				+ ", companyUniqueNo=" + companyUniqueNo + ", memberSido=" + memberSido + ", memberSigungu="
-				+ memberSigungu + ", memberBname=" + memberBname + "]";
+		return "LoginCompany [companyId=" + companyId + ", companyPw=" + companyPw + ", companyName=" + companyName
+				+ ", companyUniqueNo=" + companyUniqueNo + ", companySido=" + companySido + ", companySigungu="
+				+ companySigungu + ", companyBname=" + companyBname + "]";
 	}
-	
-
 }
