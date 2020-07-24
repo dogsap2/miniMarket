43a09f8b6ce04f429901e519d1d85deab7ec7a6e
seoiltae <sktsch11@naver.com>
2020-07-24 10:29:09 +0900

    [서일태] addReview.html 리뷰작성관련 문제 해결완료 MemberReviewController.java 	- 리뷰 작성 시 데이터가 DB에 저장되지 않는 문제로 인해 ajax처리로 변경하여 데이터 저장완료

--- miniMarket/src/main/java/com/cafe24/dk4750/miniMarket/controller/MemberReviewController.java
+++ miniMarket/src/main/java/com/cafe24/dk4750/miniMarket/controller/MemberReviewController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cafe24.dk4750.miniMarket.service.MemberReviewService;
 import com.cafe24.dk4750.miniMarket.service.MemberService;
@@ -33,17 +34,17 @@ public class MemberReviewController {
 		
 		model.addAttribute("memberNickname", memberNickname);
 		model.addAttribute("memberReview", memberReview);
-		
 		return "addReview";
 	}
 	
 	//리뷰 인서트 포스트 맵핑
 	@PostMapping("/addReview")
+	@ResponseBody
 	public void addReview(HttpSession session, MemberReview memberReview) {
 
 		System.out.println("addReview post매핑 스타트");
 		System.out.println(memberReview+"<== 리뷰 작성 하기 포스트맵핑  값확인");
-		
+		System.out.println(memberReview.getMemberItemNo()+"<--==--=-=-=멤버 리뷰정보보보보보보");
 		// addReview 서비스
 		memberReviewService.addReview(session, memberReview);
 	}
